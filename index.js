e13691ff702184b24059d47790b21ea0658641cc
Manmohan Singh <manmohan.singh@publicissapient.com>
2019-06-16 09:40:45 +0100

    babelrc file added

--- index.js
+++ index.js
@@ -2,4 +2,11 @@
  * index.js
  */
 
-console.log('Hello, world!');
+const logSomething = options => ({
+    ...options,
+    anotherOption: 'Hello!'
+});
+
+const options = logSomething({ one: '1', two: '2' });
+
+console.log(options);
