c50c9316c950187f693f2a907b93927528615cde
Manmohan Singh <manmohan.singh@publicissapient.com>
2019-06-16 23:18:38 +0100

    register-sw.js file lint issue resolved

--- src/register-sw.js
+++ src/register-sw.js
@@ -1,16 +1,16 @@
 // Initiate Service Worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js').then((reg) => {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', reg.scope);
-      }, (err) => {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      }).catch((err) => {
-        console.log(err);
-      });
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('service-worker.js').then((reg) => {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', reg.scope);
+    }, (err) => {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    }).catch((err) => {
+      console.log(err);
     });
-  } else {
-    console.log('service worker is not supported');
-  }
+  });
+} else {
+  console.log('service worker is not supported');
+}
